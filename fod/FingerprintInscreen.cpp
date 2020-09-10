a763c2b02d32ea28e2f38f94eed72607290c6fb7
Eric De Ron <OrdenKriegerEDR@googlemail.com>
2020-09-10 03:39:21 +0200

    zenfone8: Set hbm node on fp press
    Change-Id: I3fe4307ef61c289b3abea511f60d9852ddd2e114

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -16,8 +16,13 @@
 
 #define LOG_TAG "FingerprintInscreenService"
 
+#include <android-base/file.h>
 #include "FingerprintInscreen.h"
 #include <hidl/HidlTransportSupport.h>
+
+#define GLOBAL_HBM_PATH "/proc/globalHbm"
+#define GLOBAL_HBM_ON "1"
+#define GLOBAL_HBM_OFF "0"
 
 namespace vendor {
 namespace omni {
@@ -42,12 +47,14 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 Return<void> FingerprintInscreen::onPress() {
     this->mGoodixFingerprintDaemon->sendCommand(200001, {},
                                                 [](int, const hidl_vec<signed char>&) {});
+    android::base::WriteStringToFile(GLOBAL_HBM_ON, GLOBAL_HBM_PATH);
     this->mGoodixFingerprintDaemon->sendCommand(200002, {},
                                                 [](int, const hidl_vec<signed char>&) {});
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    android::base::WriteStringToFile(GLOBAL_HBM_OFF, GLOBAL_HBM_PATH);
     this->mGoodixFingerprintDaemon->sendCommand(200003, {},
                                                 [](int, const hidl_vec<signed char>&) {});
     return Void();
@@ -58,6 +65,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    android::base::WriteStringToFile(GLOBAL_HBM_OFF, GLOBAL_HBM_PATH);
     return Void();
 }
 
