726ab21bf0f7d69ce3032ee9073f024f2db88702
Vachounet <vachounet@live.fr>
2021-01-27 15:32:48 +0100

    zenfone6: build android.hidl.base@1.0 and android.hidl.manager@1.0
    Change-Id: I7d78eb4e3791ae35daba6726e0e0185cd900eebc

--- device.mk
+++ device.mk
@@ -115,6 +115,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
     libhidltransport \
     libhwbinder
 
@@ -158,9 +160,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     vndk_package
-
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
