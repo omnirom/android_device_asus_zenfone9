0bb9787593ad7283e9c55c48999727ef11297324
Carter Hsu <carterhsu@google.com>
2021-11-09 14:30:55 +0800

    audio: run audio hardware as 64bit
    [micky387] Convert to Soong config variables
    
    Bug: 205240941
    Test: regression test is passed. (b/207432169)
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ieb6ff80aa61108e18fed65ad02f9754f59a2812c
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -92,6 +92,8 @@ PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
 
 # Audio
+$(call soong_config_set,android_hardware_audio,run_64bit,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
