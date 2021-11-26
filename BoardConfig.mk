b69e4b819a98b42cf56f31714f8ee63949066999
micky387 <mickaelsaibi@free.fr>
2021-11-26 22:58:05 +0100

    zenfone8: Enable clang -fuse-ld=lld flag
    Change-Id: I25cf8b0e66c4be4c25afc2006bb1a8edc44c2072

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,7 +135,9 @@ TARGET_KERNEL_SOURCE := kernel/asus/sm8350
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
 
 # Use Gnu AS until we can switch to LLVM_IAS=1
