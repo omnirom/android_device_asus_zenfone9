46e1a016830e0b8d73eb65b6908020df009c00d0
dmd79 <davidevinavil@gmail.com>
2022-02-07 22:37:03 +0100

    zenfone8: Add blank callbacks for udfps
    Change-Id: I8e38d07f0f216810c3f5a9b7123c7d0e9a2bdcd0

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -96,6 +96,14 @@ Return<void> BiometricsFingerprint::onFingerUp() {
     return Void();
 }
 
+Return<void> BiometricsFingerprint::onShowUdfpsOverlay() {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onHideUdfpsOverlay() {
+    return Void();
+}
+
 Return<RequestStatus> BiometricsFingerprint::ErrorFilter(int32_t error) {
     switch(error) {
         case 0: return RequestStatus::SYS_OK;
