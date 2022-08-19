cc1027842e3c701a6541ae679bc89cb008d5642d
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:48:18 +0200

    zenfone9: Copy prebuilt libhidlbase from VNDK v32
    Change-Id: Ie20f3771f338d825126881fe69f034ec646def43

--- device.mk
+++ device.mk
@@ -350,6 +350,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.zenfone9
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
