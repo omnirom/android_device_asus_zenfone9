0681d3cd630160b44744a98af14e4c9a1ad05168
micky387 <mickaelsaibi@free.fr>
2022-11-03 15:46:56 +0100

    zenfone9: Use prebuilt Asus Display hal for the moment

--- device.mk
+++ device.mk
@@ -140,8 +140,6 @@ PRODUCT_PACKAGES += \
     lights.qcom
 
 $(call inherit-product, hardware/qcom-caf/sm8350/media/product.mk)
-$(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-board.mk)
-$(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.mk)
 $(call inherit-product, vendor/qcom/opensource/display/config/display-product-vendor.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys/display_custom/config/display-product-commonsys.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
