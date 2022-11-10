2829bdc45429ccb30f338e33ad48bc2fa758ed3e
micky387 <mickaelsaibi@free.fr>
2022-11-10 23:57:35 +0100

    zenfone9: Set permissive
    Change-Id: I79f60768fcd14f7d3c18b7c557ff28bfe039d3ef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,9 +135,10 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_BOOTCONFIG := |
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.selinux=permissive
 
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
 TARGET_KERNEL_ARCH := arm64
