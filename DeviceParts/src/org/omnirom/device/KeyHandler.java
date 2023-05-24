10c627b74c7cbbff84b4088e83f558ed8311d2ad
micky387 <mickaelsaibi@free.fr>
2023-05-24 18:19:53 +0200

    zenfone9: Move offscreen music gestures outside the launch camera event
    Sending an keycode by sendKeycode is not expected here.
    
    Fix this error
    
    05-24 17:41:52.096  2219  2665 E InputManager-JNI: An exception was thrown by callback 'interceptKeyBeforeQueueing'.
    05-24 17:41:52.096  2219  2665 E InputManager-JNI: java.lang.NoSuchMethodError: No static method sendKeycode(I)V in class Lorg/omnirom/omnilibcore/utils/OmniUtils; or its super classes (declaration of 'org.omnirom.omnilibcore.utils.OmniUtils' appears in /system/framework/services.jar!classes2.dex)
    05-24 17:41:52.096  2219  2665 E InputManager-JNI:      at org.omnirom.device.KeyHandler.getGestureValueForScanCode(KeyHandler.java:569)
    05-24 17:41:52.096  2219  2665 E InputManager-JNI:      at org.omnirom.device.KeyHandler.isCameraLaunchEvent(KeyHandler.java:353)
    05-24 17:41:52.096  2219  2665 E InputManager-JNI:      at com.android.server.policy.PhoneWindowManager.interceptKeyBeforeQueueing(PhoneWindowManager.java:3977)
    05-24 17:41:52.096  2219  2665 E InputManager-JNI:      at com.android.server.wm.InputManagerCallback.interceptKeyBeforeQueueing(InputManagerCallback.java:146)
    05-24 17:41:52.096  2219  2665 E InputManager-JNI:      at com.android.server.input.InputManagerService.interceptKeyBeforeQueueing(InputManagerService.java:3032
    
    Change-Id: Ief15c0d99b130fd96d997ac07457d4750652f94b

--- DeviceParts/src/org/omnirom/device/KeyHandler.java
+++ DeviceParts/src/org/omnirom/device/KeyHandler.java
@@ -320,6 +320,10 @@ public class KeyHandler implements DeviceKeyHandler {
                     mContext.startActivity(intent);
             }
         }
+        boolean value2 = getGestureValueForMusicCode(fpcode);
+        if (value2 && !mDispOn){
+            isFpgesture = true;
+        }
         return isFpgesture;
     }
 
@@ -564,20 +568,36 @@ public class KeyHandler implements DeviceKeyHandler {
             case KEY_GESTURE_Z:
                 return Settings.System.getStringForUser(mContext.getContentResolver(),
                     GestureSettings.DEVICE_GESTURE_MAPPING_5, UserHandle.USER_CURRENT);
-            case  KEY_GESTURE_PAUSE:
-                if (DEBUG) Log.i(TAG, "Music Play/Pause");
-                OmniUtils.sendKeycode(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
-                break;
-            case KEY_GESTURE_FORWARD:
-                if (DEBUG) Log.i(TAG, "Music Next");
-                OmniUtils.sendKeycode(KeyEvent.KEYCODE_MEDIA_NEXT);
-                break;
-            case KEY_GESTURE_REWIND:
-                if (DEBUG) Log.i(TAG, "Music Previous");
-                OmniUtils.sendKeycode(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
-                break;
         }
         return null;
+    }
+    
+    private boolean getGestureValueForMusicCode(int scanCode) {
+        switch(scanCode) {
+            case KEY_GESTURE_PAUSE:
+                if (DEBUG) Log.i(TAG, "Music Play/Pause");
+                    mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+                    OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
+                    dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+                return true;
+            case KEY_GESTURE_FORWARD:
+                if (DEBUG) Log.i(TAG, "Music Next");
+                if (isMusicActive()) {
+                    mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+                    OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
+                    dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_NEXT);
+                }
+                return true;
+            case KEY_GESTURE_REWIND:
+                if (DEBUG) Log.i(TAG, "Music Previous");
+                if (isMusicActive()) {
+                    mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+                    OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
+                    dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                }
+                return true;
+        }
+        return false;
     }
 
     private String getGestureValueForFPScanCode(int scanCode, long eventTime) {
