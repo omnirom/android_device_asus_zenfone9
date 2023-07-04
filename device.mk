de5d2e4573b07c1321d5d67f506de12dc1144c74
micky387 <mickaelsaibi@free.fr>
2023-07-04 13:57:16 +0200

    zenfone9: Switch to AOSP bluetooth module for hearing aid
    Change-Id: Iebb9dd13ccb251f45cad2a4d1ce2fa5e08272d49

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/usecaseKvManager_davinci.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape/usecaseKvManager_davinci.xml \
     $(LOCAL_PATH)/audio/audio_effects_AI2202.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml
 
 # Biometric
 PRODUCT_PACKAGES += \
