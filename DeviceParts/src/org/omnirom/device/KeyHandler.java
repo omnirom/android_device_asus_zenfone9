12fae1466aedd562e3bd910ec1488af75dbc2bde
micky387 <mickaelsaibi@free.fr>
2023-10-13 15:15:42 +0200

    zenfone9: Fix takeScreenshot screenshot entry
    Partial (region) screenshot is totaly dropped by Google, not sure we will add back...
    
    Change-Id: Id3f1eb83949de6d402cf9d43d9d02c70bb580443

--- DeviceParts/src/org/omnirom/device/KeyHandler.java
+++ DeviceParts/src/org/omnirom/device/KeyHandler.java
@@ -17,7 +17,6 @@
 */
 package org.omnirom.device;
 
-import static android.view.WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
 import static android.view.WindowManager.ScreenshotSource.SCREENSHOT_GLOBAL_ACTIONS;
 
 import android.app.ActivityManagerNative;
@@ -550,7 +549,7 @@ public class KeyHandler implements DeviceKeyHandler {
         } else if (value.equals(AppSelectListPreference.SCREENSHOT_ENTRY)) {
             final ScreenshotHelper screenshotHelper = new ScreenshotHelper(mContext);
             mHandler.postDelayed(() -> {
-                screenshotHelper.takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_GLOBAL_ACTIONS,
+                screenshotHelper.takeScreenshot(SCREENSHOT_GLOBAL_ACTIONS,
                     mHandler, null);
             }, 1000);
             OmniVibe.performHapticFeedbackLw(HapticFeedbackConstants.LONG_PRESS, false, mContext);
