5ea8b8025c2954021414ad37ab70779aba301262
micky387 <mickaelsaibi@free.fr>
2023-10-24 10:47:49 +0200

    zenfone9: Move frameworks res to our omnirom-res RRO
    Change-Id: I153c9bed8517c2c6316040d124ac2231ff5004ca

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     FrameworksResOverlay \
     FrameworksResVendor \
+    OmniRomResInternalOverlay \
     SettingsOverlay \
     SettingsProviderOverlay \
     SystemUIOverlay \
