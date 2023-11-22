4476e4032546cb91f5b9a6ed067469035f22284b
Michael Bestas <mkbestas@lineageos.org>
2023-11-22 19:21:47 +0200

    zenfone9: Build recovery QTI health AIDL service
    We have a recovery partition.
    
    Change-Id: I10841b26feea3c214349a20b2eedec74924f993a
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # Health for charing control
 PRODUCT_PACKAGES += \
