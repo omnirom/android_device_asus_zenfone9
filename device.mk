67f6658703f0cec370dc9ad49cbdcf5a1f7111ec
micky387 <mickaelsaibi@free.fr>
2023-12-28 03:12:34 +0100

    zenfone9: Build Kernel inline
    Change-Id: I763d5290d991c894fd4a99845312aeaad0eb8cba

--- device.mk
+++ device.mk
@@ -30,13 +30,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Prebuilt Kernel Binary
 TARGET_KERNEL_VERSION := 5.10
-TARGET_KERNEL_DIR ?= device/asus/zenfone9-kernel
-LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-# Prebuilt Kernel Headers
-TARGET_BOARD_KERNEL_HEADERS ?= device/asus/zenfone9-kernel/kernel-headers
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
