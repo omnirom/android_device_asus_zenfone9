a93f6536f409ed0d2c2cc6cfae9d561c66a0cbbd
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    zenfone9: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- Android.mk
+++ Android.mk
@@ -62,14 +62,4 @@ $(XROM_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(ASUSFW_MOUNT_POINT) $(XROM_MOUNT_POINT)
 
-WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
-$(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating WiFi firmware symlinks: $@"
-	@mkdir -p $@
-	$(hide) ln -sf /vendor/factory/wlan_mac.bin $@/wlan_mac.bin
-	$(hide) ln -sf /vendor/firmware/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
-
-ALL_DEFAULT_INSTALLED_MODULES += \
-    $(WIFI_FIRMWARE_SYMLINKS)
-
 endif
--- device.mk
+++ device.mk
@@ -369,3 +369,8 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
+
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
