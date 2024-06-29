9575809ae420b704171ae19fc13848333523a4bf
Aaron Kling <webgeek1234@gmail.com>
2024-06-28 22:09:34 -0500

    zenfone9: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
     vendor/bin/vendor.dpmd)
         "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
         ;;
+    vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
+        sed -Ei "/media_codecs_(google_audio|google_telephony|google_video|vendor_audio)/d" "${2}"
+        ;;
     esac
 }
 
