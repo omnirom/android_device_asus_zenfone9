58d2f30772bf865301209594ad51188290ebd1ea
micky387 <mickaelsaibi@free.fr>
2024-07-17 09:28:44 -0400

    zenfone9: Drop WFD libwfdnative symlink
    after https://github.com/omnirom/android_device_asus_zenfone9/commit/5dc1bcf116880911539dcb26c46f868f56610411
    
    Change-Id: I51f1fa772725e3394fb486d47e8bc0b02da4d89f

--- Android.mk
+++ Android.mk
@@ -50,15 +50,6 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
-WFD_LIB := libwfdnative.so
-WFD_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64/,$(notdir $(WFD_LIB)))
-$(WFD_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "WFD lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
-ALL_DEFAULT_INSTALLED_MODULES += $(WFD_SYMLINKS)
-
 # A/B builds require us to create the mount points at compile time.
 # Just creating it for all cases since it does not hurt.
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
