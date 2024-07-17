4054a38042b2177a17cfd383a34ce5ca2c1e716b
micky387 <mickaelsaibi@free.fr>
2024-07-17 09:39:48 -0400

    zenfone9: Convert factory mnt_point symlink to install_symlink targets
    Change-Id: I4882509a60545416223f25ccab12142e05e127a6

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ $(call inherit-product, hardware/qcom-caf/sm8450/media/product.mk)
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-V1-ndk_platform.vendor
 
+# Mount Point symlink
+PRODUCT_PACKAGES += \
+    mnt_point_factory_symlink
+
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
