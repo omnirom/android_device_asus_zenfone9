9ce3e386552894ae5c1b55c54194fbbc19ddc138
micky387 <mickaelsaibi@free.fr>
2024-07-23 12:41:14 -0400

    zenfone9: replace deprecated kcm SPECIAL_FUNCTION
    W KeyCharacterMap: The SPECIAL_FUNCTION type is now declared in the device's IDC file, please set the property 'keyboard.specialFunction' to '1' there instead.
    
    Change-Id: I2d17b3a55a0e112a7f69ee659b88c9e016f7f5cc
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fts_ts.idc:system/usr/idc/fts_ts.idc \
-    $(LOCAL_PATH)/keylayout/fts_ts.kcm:system/usr/keychars/fts_ts.kcm \
     $(LOCAL_PATH)/keylayout/fts_ts.kl:system/usr/keylayout/fts_ts.kl \
     $(LOCAL_PATH)/keylayout/goodixfp.kl:system/usr/keylayout/goodixfp.kl \
     $(LOCAL_PATH)/keylayout/i-rocks_Bluetooth_Keyboard.kl:system/usr/keylayout/i-rocks_Bluetooth_Keyboard.kl
--- keylayout/fts_ts.idc
+++ keylayout/fts_ts.idc
@@ -1,3 +1,4 @@
 device.internal = 1
+keyboard.specialFunction = 1
 touch.deviceType = touchScreen
 touch.orientationAware = 1
