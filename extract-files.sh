e64ee0d91efeec6de2e8b17e8b9e07a6a64a3962
Bruno Martins <bgcngm@gmail.com>
2024-12-04 16:18:08 -0500

    zenfone9: Address seccomp filter failures
    Fixes:
     W ATFWD-daemon: libminijail[4334]: logging seccomp filter failures
     E ATFWD-daemon: libminijail[4334]: blocked syscall: gettid
     I init    : Service 'vendor.atfwd' (pid 4334) received signal 31
     I init    : Sending signal 9 to service 'vendor.atfwd' (pid 4334) process group...
     ...
     W modemManager: libminijail[5312]: logging seccomp filter failures
     E modemManager: libminijail[5312]: blocked syscall: gettid
     I init    : Service 'vendor.modemManager' (pid 5312) received signal 31
     I init    : Sending signal 9 to service 'vendor.modemManager' (pid 5312) process group...
     ...
     W sensors-qesdk: libminijail[6807]: logging seccomp filter failures
     E sensors-qesdk: libminijail[6807]: blocked syscall: gettid
     I init    : Service 'vendor.sensors.qesdk' (pid 5312) received signal 31
     I init    : Sending signal 9 to service 'vendor.sensors.qesdk' (pid 5312) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,11 @@ function blob_fixup() {
     vendor/etc/media_codecs_cape.xml|vendor/etc/media_codecs_cape_vendor.xml)
         sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
         ;;
+    vendor/etc/seccomp_policy/atfwd@2.0.policy | \
+        vendor/etc/seccomp_policy/modemManager.policy | \
+        vendor/etc/seccomp_policy/sensors-qesdk.policy)
+        echo "gettid: 1" >> "${2}"
+        ;;
     esac
 }
 
