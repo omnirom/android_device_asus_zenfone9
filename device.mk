0bbfe0b14d79f9ba9887f1084c72064a38335593
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    zenfone9: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -110,9 +110,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
