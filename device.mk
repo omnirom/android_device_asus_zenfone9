807e97496d5a466479b87e38f6c83a937c847937
micky387 <mickaelsaibi@free.fr>
2025-02-24 17:39:42 -0500

    zenfone9: Remove Qcom display commonsys soong namespace
    Already set by qcom-caf commom repo
    
    Change-Id: Ic6462f0f8bff08a2f660a612cff92e8587ae40c4
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -254,9 +254,7 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/qcom/opensource/commonsys/display \
-    vendor/qcom/opensource/commonsys-intf/display
+    $(LOCAL_PATH)
 
 # Sensors
 PRODUCT_PACKAGES += \
