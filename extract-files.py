833d722351b60d20d008686f92664a5df5298934
micky387 <mickaelsaibi@free.fr>
2025-06-28 18:10:55 -0400

    zenfone9: Drop prebuilt qti android.hardware.audio service_64
    We already build the android.hardware.audio service from source
    
    Change-Id: I93fb2495558227213ed471638d3503abe6086851
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- extract-files.py
+++ extract-files.py
@@ -50,7 +50,6 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/com.qualcomm.location/com.qualcomm.location.apk': blob_fixup()
         .apktool_patch('blob-patches/com.qualcomm.location.patch'),
     ('vendor/bin/vendor.dpmd',
-      'vendor/bin/hw/android.hardware.audio.service_64',
       'vendor/bin/hw/vendor.qti.hardware.AGMIPC@1.0-service'): blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti',
