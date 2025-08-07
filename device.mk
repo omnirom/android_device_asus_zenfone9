ea36176b724a86b228ef0dfd0782ed2d4a8006da
Luofan Chen <clfbbn@gmail.com>
2025-08-06 21:48:30 -0400

    zenfone9: Move to common Lineage fingerprint AIDL
    Change-Id: I8bb34a7856846a5e34466b838ab24b6bd53ca623
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_COPY_FILES += \
 
 # Biometric
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint-service.lineage
 
 # Boot control
 PRODUCT_PACKAGES += \
