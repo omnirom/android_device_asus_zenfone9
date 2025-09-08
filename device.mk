292166f3a9c622a46c218370f257e66f2fdcd466
micky387 <mickaelsaibi@free.fr>
2025-09-08 18:20:01 -0400

    zenfone9: Add lineage health fast charge
    Change-Id: Ic2170ebad3f4f12cce8c28a06bc4c59eba17ec69
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -156,6 +156,11 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/asuslib/smartchg_slow_charging)
+$(call soong_config_set,lineage_health,fast_charge_value_none,10)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,18)
+$(call soong_config_set,lineage_health,fast_charge_value_super_fast_charge,0)
+
 # Init
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_zenfone9)
 
